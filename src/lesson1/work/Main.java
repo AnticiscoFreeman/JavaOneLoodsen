f45b58c8fc6a8a00a473a3ee1d2374f42c0222f0
Aleksandr Gladkov <GladkovAV@loodsen.ru>
2025-02-07 18:27:52 +0200

    add ternar operation in home1

--- src/lesson1/work/Main.java
+++ src/lesson1/work/Main.java
@@ -3,53 +3,54 @@ package lesson1.work;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ternarOperation();
 
-        byte myByte = 10; //-128..+127 -> 1byte
-        short myShort = 300; //-32768..+32767 -> 2byte
-        int myInteger = 50000; //-2.1mlrd..+2.1mlrd -> 4byte
-        long myLong = 30000000000L; //-2^63..+2^63 -> 8byte
-
-        float myFloat = 345.67f; // <>.0000000 -> 4byte
-        double myDouble = 345.67891; //<>.00000000000000 -> 8byte
-
-        char myChar = 'D'; // 2byte
-        String myString = "Word";
-        String concatString = "Word " + 123;
-
-        boolean myBooleanTrue = true; //1 byte
-        boolean myBooleanFalse = false; //1 byte
-
-        String value1 = myMethodName1();
-        System.out.println("value 1 = " + value1);
-        System.out.println("value from method = " + myMethodName1());
-        myMethodVoid();
-        myMethodWithArgs1(120);
-        int myCurrentCash = myMethodWithArgs2(120);
-        System.out.println("Result = " + myCurrentCash);
-        myMethodWithArgs3(300, "Word", 'w');
-
-        int a = 20;
-
-        if (a == 80) {
-            System.out.println("A = 20");
-        } else {
-            System.out.println("A != 20");
-        }
-
-        int b = 350;
-
-        if (b > 200) {
-            System.out.println("b > 200");
-        } else if (b > 340) {
-            System.out.println("b > 340");
-        } else {
-            System.out.println("b < 200");
-        }
-
-        int c = 30;
-        if (c < 10) {
-            System.out.println("c < 10");
-        }
+//        byte myByte = 10; //-128..+127 -> 1byte
+//        short myShort = 300; //-32768..+32767 -> 2byte
+//        int myInteger = 50000; //-2.1mlrd..+2.1mlrd -> 4byte
+//        long myLong = 30000000000L; //-2^63..+2^63 -> 8byte
+//
+//        float myFloat = 345.67f; // <>.0000000 -> 4byte
+//        double myDouble = 345.67891; //<>.00000000000000 -> 8byte
+//
+//        char myChar = 'D'; // 2byte
+//        String myString = "Word";
+//        String concatString = "Word " + 123;
+//
+//        boolean myBooleanTrue = true; //1 byte
+//        boolean myBooleanFalse = false; //1 byte
+//
+//        String value1 = myMethodName1();
+//        System.out.println("value 1 = " + value1);
+//        System.out.println("value from method = " + myMethodName1());
+//        myMethodVoid();
+//        myMethodWithArgs1(120);
+//        int myCurrentCash = myMethodWithArgs2(120);
+//        System.out.println("Result = " + myCurrentCash);
+//        myMethodWithArgs3(300, "Word", 'w');
+//
+//        int a = 20;
+//
+//        if (a == 80) {
+//            System.out.println("A = 20");
+//        } else {
+//            System.out.println("A != 20");
+//        }
+//
+//        int b = 350;
+//
+//        if (b > 200) {
+//            System.out.println("b > 200");
+//        } else if (b > 340) {
+//            System.out.println("b > 340");
+//        } else {
+//            System.out.println("b < 200");
+//        }
+//
+//        int c = 30;
+//        if (c < 10) {
+//            System.out.println("c < 10");
+//        }
 
         System.out.println("End Program");
     }
@@ -75,5 +76,37 @@ public class Main {
 
     public static void myMethodWithArgs3(int cash, String value, char myChar) {
         System.out.println(value + myChar + " = " + cash);
+    }
+
+    public static void ternarOperation() {
+        int a = 6;
+        int result = (returnBool1())
+                ? returnValue1()
+                : returnValue2();
+        System.out.println(result);
+
+        int result2;
+
+        if (returnBool1()) {
+            result2 = returnValue1();
+        } else {
+            result2 = returnValue2();
+        }
+
+    }
+
+    public static boolean returnBool1() {
+        //...
+        return true;
+    }
+
+    public static int returnValue1() {
+        //...
+        return 4;
+    }
+
+    public static int returnValue2() {
+        //...
+        return 18;
     }
 }
