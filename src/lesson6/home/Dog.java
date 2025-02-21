58ebb8da1561a8730dff1df0c14698ff906fd7e1
Aleksandr Gladkov <GladkovAV@loodsen.ru>
2025-02-21 18:31:22 +0200

    add home 6

---
+++ src/lesson6/home/Dog.java
@@ -0,0 +1,13 @@
+package lesson6.home;
+
+public class Dog extends Animal {
+
+    public static int countDog = 0;
+    private static final String TYPE = "Пес";
+
+    Dog(String name, int maxRun, int maxSwim) {
+        super(TYPE, name, maxRun, maxSwim);
+        ++countDog;
+    }
+
+}
